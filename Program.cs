1ae51c498becd937f9a06442e546060917489905
aborgra <audrey.borgra@gmail.com>
2020-02-28 14:09:25 -0600

    phase 1 complete

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlanYourHeist {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Plan Your Heist!");
+
+            Dictionary<int, string> memberName = new Dictionary<int, string> ();
+            Dictionary<int, int> memberSkillLevel = new Dictionary<int, int> ();
+            Dictionary<int, decimal> memberCourage = new Dictionary<int, decimal> ();
+
+            Console.WriteLine ("Enter team member's name");
+            memberName.Add (memberName.Count, Console.ReadLine ());
+            Console.WriteLine ("Enter member skill level (1-50)");
+            memberSkillLevel.Add (memberSkillLevel.Count, int.Parse (Console.ReadLine ()));
+            Console.WriteLine ("Enter member courage level (0.0 - 2.0)");
+            memberCourage.Add (memberCourage.Count, decimal.Parse (Console.ReadLine ()));
+
+            foreach (KeyValuePair<int, string> member in memberName) {
+                int skillLevel = memberSkillLevel[member.Key];
+                decimal courageLevel = memberCourage[member.Key];
+                Console.WriteLine ($"Name: {member.Value} Skill Level: {skillLevel} Courage Level: {courageLevel}");
+
+            }
+
+        }
+    }
+}
