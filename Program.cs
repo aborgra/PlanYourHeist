2757d4b10cd383f1ede60215eb7d40e562221537
aborgra <audrey.borgra@gmail.com>
2020-02-28 15:12:13 -0600

    phase 2 complete

--- Program.cs
+++ Program.cs
@@ -10,20 +10,24 @@ namespace PlanYourHeist {
             Dictionary<int, int> memberSkillLevel = new Dictionary<int, int> ();
             Dictionary<int, decimal> memberCourage = new Dictionary<int, decimal> ();
 
-            Console.WriteLine ("Enter team member's name");
-            memberName.Add (memberName.Count, Console.ReadLine ());
-            Console.WriteLine ("Enter member skill level (1-50)");
-            memberSkillLevel.Add (memberSkillLevel.Count, int.Parse (Console.ReadLine ()));
-            Console.WriteLine ("Enter member courage level (0.0 - 2.0)");
-            memberCourage.Add (memberCourage.Count, decimal.Parse (Console.ReadLine ()));
-
-            foreach (KeyValuePair<int, string> member in memberName) {
-                int skillLevel = memberSkillLevel[member.Key];
-                decimal courageLevel = memberCourage[member.Key];
-                Console.WriteLine ($"Name: {member.Value} Skill Level: {skillLevel} Courage Level: {courageLevel}");
-
+            while (true) {
+                Console.WriteLine ("Enter team member's name");
+                string singleMemberName = Console.ReadLine ();
+                if (singleMemberName == "") {
+                    foreach (KeyValuePair<int, string> member in memberName) {
+                        int skillLevel = memberSkillLevel[member.Key];
+                        decimal courageLevel = memberCourage[member.Key];
+                        Console.WriteLine ($"Name: {member.Value} Skill Level: {skillLevel} Courage Level: {courageLevel}");
+                    }
+                    break;
+                }
+                memberName.Add (memberName.Count, singleMemberName);
+                Console.WriteLine ("Enter member skill level (1-50)");
+                memberSkillLevel.Add (memberSkillLevel.Count, int.Parse (Console.ReadLine ()));
+                Console.WriteLine ("Enter member courage level (0.0 - 2.0)");
+                memberCourage.Add (memberCourage.Count, decimal.Parse (Console.ReadLine ()));
             }
-
         }
+
     }
 }
